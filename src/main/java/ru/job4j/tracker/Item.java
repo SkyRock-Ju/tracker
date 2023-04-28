cf3d4191746bd8274a985d72b4b443749ee7e9d1
Dulustan <skyrockpk@gmail.com>
2023-04-28 20:24:25 +0700

    acquired new tool hibernate

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,23 +1,32 @@
 package ru.job4j.tracker;
 
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Data
+
+@Entity
+@Table(name = "items")
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private int id;
-
+    @NonNull
+    @EqualsAndHashCode.Include
     private String name;
-
     private final LocalDateTime created = LocalDateTime.now();
-
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-    public Item(String name) {
-        this.name = name;
+    public Item() {
     }
 
     @Override
